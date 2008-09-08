1bb3ad3fbe060d9342ab1da7dc89f43e927a9ced
lckennedy@gmail.com <LeviCole@the-lappy-486.local>
2008-09-07 20:38:21 -0500

    what was i thinking...fixed security hole...

--- slices/merb-auth/app/controllers/users.rb
+++ slices/merb-auth/app/controllers/users.rb
@@ -1,4 +1,13 @@
 class MerbAuth::Users < MerbAuth::Application
   before :get_settings
+  before :login_required_unless_no_users
+  
+  
+  private
+  def login_required_unless_no_users
+    unless User.first.nil? && current_user
+      redirect "/"
+    end
+  end
   
 end
